0bfb222ad39bc5509011265054caa27df69a0cfa
csprats <222044220+csprats@users.noreply.github.com>
2025-08-01 10:40:45 +0200

    Create the moles, the score and the time

---
+++ src/main.js
@@ -0,0 +1,34 @@
+const squares = document.querySelectorAll('.square')
+const mole = document.querySelector('.mole')
+const timeleft = document.getElementById('time')
+const score = document.getElementById('score')
+
+let result = 0
+let hitPosition = 0
+
+function moveMole() {
+  squares.forEach(square => {
+    square.classList.remove('mole')
+  })
+
+  let randomSquare = squares[Math.floor(Math.random() * 9)]
+  randomSquare.classList.add('mole')
+  hitPosition = randomSquare.id
+}
+
+squares.forEach(square => {
+  square.addEventListener('click', () => {
+      if (square.id == hitPosition) {
+        result++
+        score.textContent = result
+        moveMole()
+      }
+  })
+})
+
+function quitTime() {
+  timeleft.textContent--
+}
+
+setInterval(quitTime, 1000)
+moveMole()
